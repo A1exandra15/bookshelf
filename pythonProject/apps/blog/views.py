86f7b27ddfa80218011b6d183525b9ad9ac62cba
A1exandra15 <alexandra.korchunova@gmail.com>
2022-08-01 17:34:21 +0300

    lesson 11

--- pythonProject/apps/blog/views.py
+++ pythonProject/apps/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from apps.blog.models import BlogCategory, Article
 
-# Create your views here.
+
+def blog_categery_list(request):
+    blog_categeries = BlogCategory.objects.all()
+    return render(request, 'blog/category/list.html', {'categeries': blog_categeries})
+
+
+def article_list(request, category_id):
+    articles = Article.objects.filter(category=category_id)
+    return render(request, 'blog/article/list.html', {'articles': articles})
+
+def article_view(request, category_id, article_id):
+    article = Article.objects.get(id=article_id)
+    category = BlogCategory.objects.get(id=category_id)
+    return render(request, 'blog/article/view.html', {'article': article, 'category': category})

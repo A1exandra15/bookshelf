0ef4ad60d1d738d6ce37d311cb87e4c6d65f81e6
A1exandra15 <alexandra.korchunova@gmail.com>
2022-08-05 12:27:10 +0300

    bootstrap, static

--- pythonProject/config/urls.py
+++ pythonProject/config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from config.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('apps.blog.urls')),
+
 ]
 

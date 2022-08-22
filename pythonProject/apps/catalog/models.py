cef1d1decb02ab02cb802d05132a533f0ee7387f
A1exandra15 <alexandra.korchunova@gmail.com>
2022-08-22 11:02:47 +0300

    catalog product

--- pythonProject/apps/catalog/models.py
+++ pythonProject/apps/catalog/models.py
@@ -7,58 +7,44 @@ from django.urls import reverse
 from config.settings import MEDIA_ROOT
 
 
-class Product(models.Model):
-    name = models.CharField(verbose_name='Название', max_length=255)
-    description = models.TextField(verbose_name='Описание', blank=True, null=True)
-    quantiti = models.IntegerField(verbose_name='Количество', null=True)
-    price = models.DecimalField(verbose_name='Цена', max_digits=12, decimal_places=2, default=0)
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name='Дата редоктиравания', auto_now=True)
-
-    class Meta:
-        verbose_name = 'Единицу товара'
-        verbose_name_plural = 'Товар'
-
-
 class Category(MPTTModel):
     name = models.CharField(verbose_name='Название', max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     parent = TreeForeignKey(
         to='self',
+        related_name='child',
+        on_delete=models.CASCADE,
         blank=True,
         null=True,
-        related_name='child',
-        on_delete=models.CASCADE
     )
-
     image = ProcessedImageField(
         verbose_name='Изображение',
-        upload_to='blog/category/',
+        upload_to='blog/category',
         processors=[ResizeToFill(600, 400)],
         format='JPEG',
-        options={'qualiti': 100},
+        options={'quality': 100},
         blank=True,
         null=True
     )
 
     def image_tag_thumbnail(self):
         if self.image:
-            return mark_safe(f'<img src="/{MEDIA_ROOT}{self.image}" width="70">')
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}' width='70'>")
 
     image_tag_thumbnail.short_description = 'Текущее изображение'
     image_tag_thumbnail.allow_tags = True
 
     def image_tag(self):
         if self.image:
-            return mark_safe(f'<img src="/{MEDIA_ROOT}{self.image}">')
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}'>")
 
     image_tag.short_description = 'Текущее изображение'
     image_tag.allow_tags = True
 
     class Meta:
-        verbose_name = 'Категория блога'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         full_path = [self.name]
@@ -70,3 +56,44 @@ class Category(MPTTModel):
 
     def get_absolute_url(self):
         return reverse('category', args=[self.slug])
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=255)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+    price = models.DecimalField(verbose_name='Цена', max_digits=12, decimal_places=2, default=1000000)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
+    categories = models.ManyToManyField(
+        to=Category,
+        verbose_name='Категории',
+        through='ProductCategory',
+        related_name='categories',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(to=Product, verbose_name='Товар', on_delete=models.CASCADE)
+    category = models.ForeignKey(to=Category, verbose_name='Категория', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основная категория', default=False)
+
+    def __str__(self):
+        return ''
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.is_main:
+            ProductCategory.objects.filter(product=self.product).update(is_main=False)
+        super(ProductCategory, self).save(force_insert, force_update, using, update_fields)
+
+    class Meta:
+        verbose_name = 'Категория товара'
+        verbose_name_plural = 'Категории товара'

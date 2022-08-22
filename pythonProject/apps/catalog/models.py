71d2cd45b889229e20236a3f44f05f0023f9a4a0
A1exandra15 <alexandra.korchunova@gmail.com>
2022-08-22 12:45:54 +0300

    hometask 15

--- pythonProject/apps/catalog/models.py
+++ pythonProject/apps/catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.safestring import mark_safe
-from imagekit.models import ProcessedImageField
+from imagekit.models import ProcessedImageField, ImageSpecField
 from mptt.models import MPTTModel, TreeForeignKey
 from pilkit.processors import ResizeToFill
 from django.urls import reverse
@@ -58,6 +58,46 @@ class Category(MPTTModel):
         return reverse('category', args=[self.slug])
 
 
+class Image(models.Model):
+    image = ProcessedImageField(
+        verbose_name='Изображение',
+        upload_to='catalog/product',
+        processors=[],
+        format='JPEG',
+        options={'qualiti': 100},
+        null=True
+    )
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(600, 400)],
+        format='JPEG',
+        options={'qualiti': 100}
+    )
+    product = models.ForeignKey(to='Product', verbose_name='Товар', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основное изображение', default=False)
+
+    def image_tag_thumbnail(self):
+        if self.image:
+            if not self.image_thumbnail:
+                Image.objects.get(id=self.id)
+            return mark_safe(f'<img src="/{MEDIA_ROOT}{self.image_thumbnail}" width="70">')
+
+    image_tag_thumbnail.short_description = 'Текущее изображение'
+    image_tag_thumbnail.allow_tags = True
+
+    def image_tag(self):
+        if self.image:
+            if not self.image_thumbnail:
+                Image.objects.get(id=self.id)
+            return mark_safe(f'<img src="/{MEDIA_ROOT}{self.image_thumbnail}">')
+
+    image_tag.short_description = 'Текущее изображение'
+    image_tag.allow_tags = True
+
+    def __str__(self):
+        return ''
+
+
 class Product(models.Model):
     name = models.CharField(verbose_name='Название', max_length=255)
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
@@ -74,11 +114,18 @@ class Product(models.Model):
     )
 
     class Meta:
+        ordering = ['-created_at']
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
 
     def __str__(self):
         return self.name
+
+    def images(self):
+        return Image.objects.filter(product=self.id)
+
+    def main_image(self):
+        return Image.objects.filter(product=self.id, is_main=True).first()
 
 
 class ProductCategory(models.Model):

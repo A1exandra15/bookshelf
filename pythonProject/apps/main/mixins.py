90bd59eb5966618ee68841be963371d8ce224085
A1exandra15 <alexandra.korchunova@gmail.com>
2022-08-25 09:34:35 +0300

    hometask 17

--- pythonProject/apps/main/mixins.py
+++ pythonProject/apps/main/mixins.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class MetaTagMixin(models.Model):
     name = None
-    meta_title = models.CharField(verbose_name="Meta Title", max_length=255, null=True, blank=True)
-    meta_description = models.TextField(verbose_name="Meta Description", null=True, blank=True)
-    meta_keywords = models.CharField(verbose_name="Meta Keywords", max_length=255, null=True, blank=True)
+    meta_title = models.CharField(verbose_name='Meta Title', max_length=255, null=True, blank=True)
+    meta_description = models.TextField(verbose_name='Meta Description', null=True, blank=True)
+    meta_keywords = models.CharField(verbose_name='Meta Keywords', max_length=255, null=True, blank=True)
 
     def get_meta_title(self):
         if self.meta_title:

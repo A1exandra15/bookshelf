f813201bf456114440ab360806114e0cefd23620
A1exandra15 <alexandra.korchunova@gmail.com>
2022-09-07 23:38:47 +0300

    homework 20

---
+++ pythonProject/apps/order/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.order import views
+
+
+urlpatterns = [
+    path('add/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_product_list, name='cart_product_list'),
+]
